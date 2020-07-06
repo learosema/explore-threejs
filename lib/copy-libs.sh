0bb95e08ca0677c5adaf2bdb2200e9bd88fb54aa
Lea Rosema <terabaud@gmail.com>
2020-07-06 23:54:33 +0200

    chore: add something work in progress

--- lib/copy-libs.sh
+++ lib/copy-libs.sh
@@ -4,4 +4,14 @@ cp ../node_modules/three/LICENSE .
 cp ../node_modules/three/build/three.module.js .
 cp -r ../node_modules/three/examples/jsm/controls .
 
-# TODO: patch the imports of the examples...
+# TODO: patch the imports of the controls...
+# "../../../build/three.module.js" to "../three.module.js"
+# '../../../src/Three' to '../three.module.js';
+
+# experimental folder
+# "../../../../build/three.module.js" to '../../three.module.js'
+
+cd controls
+# sed -i '*.bak'
+# https://stackoverflow.com/questions/11392478/how-to-replace-a-string-in-multiple-files-in-linux-command-line
+# tooo tired....
